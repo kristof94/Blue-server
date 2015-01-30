b1e5792a460dfbc5549a453e469ad6e10b90eaf4
kristof94 <christophe-dossantos@outlook.com>
2015-01-30 08:28:53 +0100

    sd

--- src/blue_server/ProcessConnectionThread.java
+++ src/blue_server/ProcessConnectionThread.java
@@ -1,48 +1,90 @@
 package blue_server;
 
+import java.awt.AWTException;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.awt.Robot;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
 
 import javax.microedition.io.StreamConnection;
 
 public class ProcessConnectionThread implements Runnable {
-	InputStream s = null;
+	DataInputStream s = null;
 	StringBuffer b = new StringBuffer();
 	StreamConnection connection;
 	BufferedReader r;
 	StringBuilder total;
-
+	boolean v=true;
+	Robot robot;
+	GraphicsDevice gd;
 	public ProcessConnectionThread(StreamConnection c) {
 		try {
 			connection = c;			
-			s = connection.openInputStream();
+			s = connection.openDataInputStream();
 			r = new BufferedReader(new InputStreamReader(s));
 			total = new StringBuilder();
+			try {
+				robot = new Robot();
+			} catch (AWTException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
 	}
 
 	@Override
 	public void run() {
-		while(true)
+
+		ByteBuffer tab = ByteBuffer.allocate(Integer.SIZE);
+
+		int width = gd.getDisplayMode().getWidth();
+		int height = gd.getDisplayMode().getHeight();
+		int x=width/2,y=height/2;
+		int ratiox=1920/width;
+		int ratioy=1080/height;
+		PointerInfo a = MouseInfo.getPointerInfo();
+		Point bb = a.getLocation();
+
+		System.out.println("width = "+width);
+		System.out.println("height = "+height);
+
+		
+		while(v)
 		{
-			String line;
 			try {
-				while ((line = r.readLine()) != null) {
-					total.append(line);
-					System.out.println(total.toString());
+
+				while(s.read(tab.array())>0)
+				{
 					
+					
+					x =( (tab.asIntBuffer().get() * ratiox) );
+					y = ( (tab.asIntBuffer().get(4) * ratioy) );
+					System.out.print("x = "+x);
+					System.out.println(" ; y = "+y);
+					//robot.mouseMove((int)bb.getX() + x  ,(int)bb.getY()  + y );
+
 				}
+				v=!v;
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
 		}
+		System.out.println("client deconnecté");
 	}
 }
